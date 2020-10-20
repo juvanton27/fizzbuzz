43105b317d6c19f92f0e98a004019af3c600eb7e
Julien Van Tongerloo <julien.vantongerloo@student.vinci.be>
2020-10-20 16:44:22 +0200

    add - count for input 1

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -6,4 +6,9 @@ class FizzBuzz
     public function play(int $number){
 
     }
+
+    public function count(int $int): string
+    {
+        return "1";
+    }
 }
--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -8,4 +8,14 @@ use PHPUnit\Framework\TestCase;
 class FizzBuzzTest extends TestCase
 {
 
+    public function  test_count_given1_shouldReturn1(){
+        //Arrange
+        $fizzBuzz = new FizzBuzz();
+
+        //Act
+        $actual = $fizzBuzz->count(1);
+
+        //Assert
+        $this->assertEquals("1", $actual);  //Exemple : $this->assertTrue(true);$this->assertEquals(true, true);
+    }
 }
