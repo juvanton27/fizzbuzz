2620d5fa7bb8c39355782136ffbded023c86e8bf
Julien Van Tongerloo <julien.vantongerloo@student.vinci.be>
2020-10-20 17:44:38 +0200

    refactor - manage multiple of 5 with a modulo

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -7,7 +7,7 @@ class FizzBuzz
     public function count(int $int): string
     {
         if($this->isMultipleOf3($int)) return "Fizz";
-        if($int === 5 || $int === 10) return "Buzz";
+        if($int%5 === 0) return "Buzz";
 
         return (string) $int;
     }
