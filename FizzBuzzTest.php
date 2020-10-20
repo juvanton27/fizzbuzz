ec3333e2d850745a8fdb073a57942d230c548d67
Julien Van Tongerloo <julien.vantongerloo@student.vinci.be>
2020-10-20 17:13:26 +0200

    refactor - externalize the fizzbuzz instance as an attribut

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -7,22 +7,21 @@ use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
+    private $fizzBuzz;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->fizzBuzz = new FizzBuzz();
+    }
 
     public function  test_count_given1_shouldReturn1(){
-        //Arrange
-        $fizzBuzz = new FizzBuzz();
+        $actual = $this->fizzBuzz->count(1);
 
-        //Act
-        $actual = $fizzBuzz->count(1);
-
-        //Assert
         $this->assertEquals("1", $actual);  //Exemple : $this->assertTrue(true);$this->assertEquals(true, true);
     }
 
-    public function test_count_given2_shouldReturn2(){
-        $fizzBuzz = new FizzBuzz();
-
-        $actual = $fizzBuzz->count(2);
+    public function test_count_given2_shouldReturn2(){$actual = $this->fizzBuzz->count(2);
 
         $this->assertEquals("2", $actual);
     }
