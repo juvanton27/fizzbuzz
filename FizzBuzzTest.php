00600b45daba3f4a9f5809d0e2a6bd6d37935434
Julien Van Tongerloo <julien.vantongerloo@student.vinci.be>
2020-10-20 17:34:02 +0200

    refactor - extract actCount

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -16,32 +16,37 @@ class FizzBuzzTest extends TestCase
     }
 
     public function  test_count_given1_shouldReturn1(){
-        $actual = $this->fizzBuzz->count(1);
+        $actual = $this->actCount(1);
 
         $this->assertEquals("1", $actual);  //Exemple : $this->assertTrue(true);$this->assertEquals(true, true);
     }
 
     public function test_count_given2_shouldReturn2(){
-        $actual = $this->fizzBuzz->count(2);
+        $actual = $this->actCount(2);
 
         $this->assertEquals("2", $actual);
     }
 
     public function test_count_given3_shouldReturnFizz(){
-        $actual = $this->fizzBuzz->count(3);
+        $actual = $this->actCount(3);
 
         $this->assertEquals("Fizz", $actual);
     }
 
     public function test_count_given4_shouldReturn4(){
-        $actual = $this->fizzBuzz->count(4);
+        $actual = $this->actCount(4);
 
         $this->assertEquals("4", $actual);
     }
 
     public function test_count_given5_shouldReturnBuzz(){
-        $actual = $this->fizzBuzz->count(5);
+        $actual = $this->actCount(5);
         
         $this->assertEquals("Buzz", $actual);
+    }
+
+    public function actCount(int $int): string
+    {
+        return $this->fizzBuzz->count($int);
     }
 }
